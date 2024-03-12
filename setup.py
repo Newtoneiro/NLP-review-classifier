c08d55823394feeeafc77cb71750cdcdfbba319c
Newton <latosek.bar@gmail.com>
2024-03-12 21:39:06 +0100

    cookiecutter init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to train a model which will classify hotel reviews as number of stars',
+    author='Bartosz Latosek, Mateusz Krakowski, Mikołaj Bańkowski',
+    license='BSD-3',
+)
